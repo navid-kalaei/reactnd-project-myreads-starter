1a166508e31d934bbcf19a5cd63abe89a7d9646b
Navid Kalaei <navidkalaei@gmail.com>
2017-09-05 14:44:58 +0430

    refactor: BookShelf to render books dynamically

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -13,12 +13,11 @@ class BookShelf extends Component {
             <h2 className="bookshelf-title">{this.props.title}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    <Book url="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api" title="Golabi" authors="Golabi Nezhad"/>
-                    {/*{this.props.books.map((book) => (*/}
-                        {/*<li>*/}
-                            {/*<Book url={book.url} title={book.title} authors={book.authors}/>*/}
-                        {/*</li>*/}
-                    {/*))}*/}
+                    {this.props.books.map((book) => (
+                        <li>
+                            <Book url={book.url} title={book.title} authors={book.authors}/>
+                        </li>
+                    ))}
                 </ol>
             </div>
         </div>
