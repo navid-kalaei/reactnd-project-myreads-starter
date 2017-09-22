a374f71a6be68ed939088b1bcd73f2b23a8aeca4
Navid Kalaei <navidkalaei@gmail.com>
2017-09-22 14:17:08 +0330

    refactor: Home to stateless function

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@
  * @exports {component} Home - The home route
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf';
 
-class Home extends Component {
+function Home(props) {
 
-    shelves = [
+    const shelves = [
         {
             id: "currentlyReading",
             title: "Currently Reading"
@@ -24,31 +24,29 @@ class Home extends Component {
         }
     ];
 
-    render() {
-        return (
-            <div className="app">
+    return (
+        <div className="app">
 
-                <div className="list-books">
-                    <div className="list-books-title">
-                        <h1>MyReads</h1>
-                    </div>
-                    <div className="list-books-content">
-                        <div>
-                            {this.shelves.map((shelf) => (
-                                <BookShelf key={shelf.title} title={shelf.title}
-                                           books={this.props.books.filter((book) => book.shelf === shelf.id)}
-                                           onChangingShelfOfBook={this.props.onChangingShelfOfBook}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="open-search">
-                        <Link to="/search">Add a book</Link>
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {shelves.map((shelf) => (
+                            <BookShelf key={shelf.title} title={shelf.title}
+                                       books={props.books.filter((book) => book.shelf === shelf.id)}
+                                       onChangingShelfOfBook={props.onChangingShelfOfBook}
+                            />
+                        ))}
                     </div>
                 </div>
+                <div className="open-search">
+                    <Link to="/search">Add a book</Link>
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Home;
