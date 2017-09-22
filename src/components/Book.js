1c2203ac5be758b8e8b6d01592e3d518fbbab074
Navid Kalaei <navidkalaei@gmail.com>
2017-09-22 14:14:49 +0330

    refactor: Book to stateless function

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,37 +3,35 @@
  * @exports {component} a single book
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import BookShelfChanger from './BookShelfChanger';
 
-class Book extends Component {
+function Book(props) {
+    const book = props.book;
 
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onChangingShelfOfBook: PropTypes.func.isRequired
-    };
+    const authors = book.authors || ['Author not available'];
+    const thumbnailNotAvailableUrl = './thumbnail-not-available.jpg';
 
-    thumbnailNotAvailableUrl = './thumbnail-not-available.jpg';
-
-    render() {
-        const book = this.props.book;
-        const authors = book.authors || ['Author not available'];
-
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover"
-                         style={{backgroundImage: `url(${book.imageLinks.thumbnail || this.thumbnailNotAvailableUrl})`}}></div>
-                    <BookShelfChanger book={book} onChangingShelfOfBook={this.props.onChangingShelfOfBook}/>
-                </div>
-                <div className="book-title">{book.title}</div>
-                {authors.map((author) => (
-                    <div key={author} className="book-authors">{author}</div>
-                ))}
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover"
+                     style={{backgroundImage: `url(${book.imageLinks.thumbnail || thumbnailNotAvailableUrl})`}}></div>
+                <BookShelfChanger book={book} onChangingShelfOfBook={props.onChangingShelfOfBook}/>
             </div>
-        );
-    }
+            <div className="book-title">{book.title}</div>
+            {authors.map((author) => (
+                <div key={author} className="book-authors">{author}</div>
+            ))}
+        </div>
+    );
 }
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    onChangingShelfOfBook: PropTypes.func.isRequired
+};
+
 
 export default Book;
