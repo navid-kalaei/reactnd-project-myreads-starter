a6c9f0e05e26254ed73f1a1e99e571ad40c6c40d
Navid Kalaei <navidkalaei@gmail.com>
2017-09-22 14:47:31 +0330

    refactor: breaking down defaults.js

--- src/conf/defaults.js
+++ src/conf/defaults.js
@@ -1,26 +1,26 @@
-export default {
-    shelves: [
-        {
-            id: "currentlyReading",
-            title: "Currently Reading"
-        },
-        {
-            id: "wantToRead",
-            title: "Want to Read"
-        },
-        {
-            id: "read",
-            title: "Read"
-        }
-    ],
-    messages: [
-        {
-            authorNotAvailable:"Author not available"
-        }
-    ],
-    urls: [
-        {
-            thumbnailNotAvailableUrl: "./thumbnail-not-available.jpg"
-        }
-    ]
-}
+export const shelves = [
+    {
+        id: "currentlyReading",
+        title: "Currently Reading"
+    },
+    {
+        id: "wantToRead",
+        title: "Want to Read"
+    },
+    {
+        id: "read",
+        title: "Read"
+    }
+];
+
+export const messages = [
+    {
+        authorNotAvailable:"Author not available"
+    }
+];
+
+export const urls = [
+    {
+        thumbnailNotAvailableUrl: "./thumbnail-not-available.jpg"
+    }
+]
