8d1ef87c4ce28ecee3183925f905753cd465d43b
Navid Kalaei <navidkalaei@gmail.com>
2017-09-22 14:09:59 +0330

    fix: store the changed shelf in BookShelfChanger

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -3,36 +3,48 @@
  * @exports {component} menu with change options
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 
-function BookShelfChanger(props) {
+class BookShelfChanger extends Component {
 
-    return (
-        <div className="book-shelf-changer">
-            <select value={props.book.shelf || 'none'}
-                    onChange={(event) => {
-                        const currentBook = props.book;
-                        const newShelf = event.target.value;
-                        props.onChangingShelfOfBook(currentBook, newShelf);
-                        }
-                    }>
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        onChangingShelfOfBook: PropTypes.func.isRequired
+    };
 
-                <option disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-            </select>
-        </div>
-    );
+    state = {
+        value: ''
+    };
+
+    changeValue(newValue) {
+        this.setState({value: newValue});
+    };
+
+    render() {
+        const book = this.props.book;
+        const onChangingShelfOfBook = this.props.onChangingShelfOfBook;
+
+        return (
+            <div className="book-shelf-changer">
+                <select value={this.state.value || book.shelf || 'none'}
+                        onChange={(event) => {
+                            const newShelf = event.target.value;
+                            this.setState({value: newShelf});
+                            onChangingShelfOfBook(book, newShelf);
+                        }}
+                    >
+
+                    <option disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+        );
+    }
 }
-
-BookShelfChanger.propTypes = {
-    book: PropTypes.object.isRequired,
-    onChangingShelfOfBook: PropTypes.func.isRequired
-};
-
 
 export default BookShelfChanger;
